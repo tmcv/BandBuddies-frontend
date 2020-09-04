4e66222c291801a8d14da17a8c9cf6163031e24a
tom <tomvosmusic@gmail.com>
2020-09-04 17:59:18 +0200

    set up frontend repo, created first basic component Homepage

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from "react-router-dom";
+import {Provider} from "react-redux";
+// import store from "./store";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>
+  ,
   document.getElementById('root')
 );
 
