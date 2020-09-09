1218c11254a4bcd7f796e4dabfbaf210873c423c
tom <tomvosmusic@gmail.com>
2020-09-09 12:42:14 +0200

    added styles and instruments to be shown on profile page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -3,13 +3,23 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 
 export default function Profile(props) {
+  console.log("PROPS:", props)
   const userType = props.isBand ? "Band or Group" : "Individual Musician"
+  const instrumentsList = props.instruments ? props.instruments : []
+  const stylesList = props.styles ? props.styles : []
+
 
   return (
     <Jumbotron>
       <h1>Name: {props.name}</h1>
-      <p>Level: {props.level}</p>
       <p>Type: {userType}</p>
+      <ul><strong>Instruments:</strong> {instrumentsList.map(i => {
+        return <li>{i.title}</li>
+      })}</ul>
+      <p>Level: {props.level}</p>
+      <ul><strong>Styles:</strong> {stylesList.map(s => {
+        return <li>{s.title}</li>
+      })}</ul>
       {/* {props.showLink ? (
         <Link to={`/listings/${props.id}`}>
           <Button>Contact User</Button>
--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -7,7 +7,8 @@ import { selectUser } from "../../store/user/selectors";
 
 export default function ProfilePage() {
   const user = useSelector(selectUser);
-
+  const levelsList = [null, 'beginner', 'intermediat', 'advanced', 'semi-professional', 'professional']
+  const userLevel = `${levelsList[user.level]}`
   console.log('USER DETAILS', user)
   
   return (
@@ -18,7 +19,7 @@ export default function ProfilePage() {
          name={user.name}
          instruments={user.instruments}
          styles={user.styles}
-         level={user.level}
+         level={userLevel}
          isBand={user.isBand}
          showLink={true}
       /> : <p>Loading...</p>}
