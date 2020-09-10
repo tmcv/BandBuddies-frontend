c77f5be3caebf7f2815b6ab853a62bd10e0cfb59
tom <tomvosmusic@gmail.com>
2020-09-10 22:54:04 +0200

    added some stuff to the Homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Button from "react-bootstrap/Button";
+import {useHistory} from "react-router-dom";
 
 export default function Homepage() {
+  const history = useHistory()
+
+  function clickFunction() {
+    history.push("/listings")
+  }
+
   return (
     <div>
-      just testing...
+      <div>
+        <h1>Welcome to Band Buddies!</h1>
+      </div>
+      <img height={720} src="https://cdn.pixabay.com/photo/2017/07/31/22/32/concert-2561646_1280.jpg" alt="Image of band performing" />
+      <p></p>
+      <div>
+        <Button onClick={clickFunction}>
+          Find A Band!
+        </Button>      
+      </div>
     </div>
   )
 }
