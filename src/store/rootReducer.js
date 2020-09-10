d79638947ced8bceb355b5b1747e4c7f29a6859a
tom <tomvosmusic@gmail.com>
2020-09-10 22:54:32 +0200

    added all the new changes to App.js and rootreducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import appState from "./appState/reducer";
 import user from "./user/reducer";
 import listings from "./listings/reducer";
+import listingDetails from "./listingDetails/reducer";
 import styles from "./styles/reducer";
 import instruments from "./instruments/reducer";
 
@@ -9,6 +10,7 @@ export default combineReducers({
   appState,
   user,
   listings,
+  listingDetails,
   styles,
   instruments
 });
